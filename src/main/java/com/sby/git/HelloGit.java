b0e80bb15947d044d6637dae94be48cd39c8fa8c
sby1 <1152102458@qq.com>
2020-07-17 15:05:51 +0800

    分支添加

--- src/main/java/com/sby/git/HelloGit.java
+++ src/main/java/com/sby/git/HelloGit.java
@@ -2,8 +2,10 @@ package com.sby.git;
 
 public class HelloGit {
 	public static void main(String[] args) {
+		System.out.println("分支添加");
 		System.out.println("Hello Git!");
 		System.out.println("更新1");
 		System.out.println("创建分支");
+		
 	}
 }
